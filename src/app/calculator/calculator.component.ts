eef5f7cf0c5b48e5e3fbe19944b0e738e5c6e0ec
Florian Dietrich <florian.dietrich@zimmer-group.com>
2022-11-03 14:50:18 +0100

    first commit

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,163 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ArgumentOutOfRangeError } from 'rxjs';
+
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  currentNumber: string = '0';
+  firstOperand: number = 0;
+  operator: string = '';
+
+  waitForSecondNumber: boolean = false;
+  isNegativ:boolean = false;
+  sqRootFlag: boolean = true;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    console.log(event.key)
+
+    if(Number(event.key)){
+      this.getNumber(event.key);
+    }
+    else if(event.key === 'Escape'){
+      this.clear();
+    }
+    else{
+      this.getOperation(event.key);
+    }
+  }
+    
+
+  public getNumber(v: string){
+    console.log(v);
+    
+    if(this.waitForSecondNumber)
+    {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    }
+    else{
+      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+    }
+
+    if(this.isNegativ){
+      this.currentNumber = '-' + this.currentNumber;
+    }
+    else{
+      this.currentNumber = this.currentNumber.replace('-','');
+    }
+  }
+
+
+  public getOperation(op: string){
+    console.log(op);
+    this.isNegativ = false;
+
+    if(this.firstOperand === 0){ //null
+      this.firstOperand = Number(this.currentNumber);
+
+    }
+    else if(this.operator){
+      const result = this.doCalculation(this.operator , Number(this.currentNumber))
+      this.currentNumber = String(result);
+      this.firstOperand = Number(result);
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+
+  }
+
+
+  public setNegativ(){
+    this.isNegativ = !this.isNegativ;
+    console.log(this.isNegativ);
+  }
+
+
+  // public getOperation(op: string){
+  //   console.log(op);
+
+  //   if(op === '-'){      // <<<
+  //     this.currentNumber = op;             
+  //   }
+  //   // frisch geändert
+  //   if(op === '-' && this.firstOperand !== 0){
+  //     let localNum = String(this.firstOperand);
+  //     let localStrTemp = this.currentNumber + localNum ;
+  //     this.firstOperand = Number(localStrTemp);
+  //   }
+  //   else if(this.operator){
+  //     const result = this.doCalculation(this.operator, Number(this.currentNumber))
+  //     this.currentNumber = String(result);
+  //     this.firstOperand = result!;
+  //   }
+  //   this.operator = op;
+  //   this.waitForSecondNumber = true;
+
+  //   console.log(this.firstOperand);
+
+  // }
+
+
+  getDecimal(){
+    if(!this.currentNumber.includes('.')){
+        this.currentNumber += '.'; 
+    }
+  }
+
+
+  private doCalculation(op: string , secondOp: number){
+    switch (op){
+      case '+':
+        return this.firstOperand += secondOp; 
+      
+      case '-': 
+        return this.firstOperand -= secondOp; 
+      
+      case '*': 
+        return this.firstOperand *= secondOp; 
+      
+      case '/': 
+        return this.firstOperand /= secondOp; 
+      
+      case '=':
+        return secondOp;
+      
+      case 'sqrt':
+        return ""+  this.sqRoot();
+
+      default: return "error"
+    }
+  }
+
+  public sqRoot(){
+    let retnumber = Math.sqrt(Number(this.currentNumber));
+    this.clear();
+    this.currentNumber = String(retnumber);
+  }
+
+
+  public clear(){
+    this.currentNumber = '0';
+    this.firstOperand = 0;
+    this.operator = '';
+    this.waitForSecondNumber = false;
+    this.isNegativ = false;
+  }
+
+
+}
