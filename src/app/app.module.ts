782a9416fbd5463384fbde63c960f953b3f418da
Florian Dietrich <florian.dietrich@zimmer-group.com>
2024-08-21 10:24:01 +0200

    adding materials and a toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToolbarComponent
+],
+  providers: [
+    provideAnimationsAsync()
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
